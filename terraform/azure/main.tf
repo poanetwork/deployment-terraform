b03df0099aee416977e7eb11a04553fffaff4d12
Aleksey Zalesov <aleksey.zalesov@altoros.com>
2018-02-15 15:17:37 +0300

    Configure the security group for bootnode

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -56,6 +56,58 @@ resource "azurerm_network_security_group" "bootnode" {
         destination_address_prefix = "*"
     }
 
+    security_rule {
+        name                       = "HTTPS"
+        priority                   = 1002
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "443"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    security_rule {
+        name                       = "RPC"
+        priority                   = 1003
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "8545"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    security_rule {
+        name                       = "P2P-TCP"
+        priority                   = 1004
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "30303"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags {
+        environment = "Terraform Demo"
+    }
+
+    security_rule {
+        name                       = "P2P-UDP"
+        priority                   = 1005
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "udp"
+        source_port_range          = "*"
+        destination_port_range     = "30303"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
     tags {
         environment = "Terraform Demo"
     }
