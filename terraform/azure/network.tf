c5ad418914ae0fac3668a85f4a7819464d421b88
Aleksey Zalesov <aleksey.zalesov@altoros.com>
2018-02-21 09:53:52 +0300

    Script creates resource group Fixed #6

--- terraform/azure/network.tf
+++ terraform/azure/network.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_network" "default" {
     name                = "${var.prefix}-default"
     address_space       = ["10.0.0.0/16"]
     location            = "${var.region}"
-    resource_group_name = "${data.azurerm_resource_group.test.name}"
+    resource_group_name = "${azurerm_resource_group.test.name}"
 
     tags {
         environment = "Terraform Demo"
@@ -13,7 +13,7 @@ resource "azurerm_virtual_network" "default" {
 # Create subnet
 resource "azurerm_subnet" "default" {
     name                 = "${var.prefix}-default"
-    resource_group_name  = "${data.azurerm_resource_group.test.name}"
+    resource_group_name  = "${azurerm_resource_group.test.name}"
     virtual_network_name = "${azurerm_virtual_network.default.name}"
     address_prefix       = "10.0.1.0/24"
 }
