ddfb32f20daac81d614ce2041dd7260e667053fe
Aleskey Zalesov <aleksey.zalesov@altoros.com>
2018-03-03 17:28:16 +0300

    Change netstat example

--- azure-refactored/examples/netstat/main.tf
+++ azure-refactored/examples/netstat/main.tf
@@ -8,8 +8,19 @@ module "common" {
 }
 
 module "netstat" {
-  source = "./poanetwork/modules/netstat"
+  source = "./poanetwork/modules/node"
 
   resource_group_name = "${module.common.resource_group_name}"
   subnet_id = "${module.common.subnet_id}"
+
+  network_name = "sokol"
+  platform = "ubuntu"
+  role = "netstat"
+
+  config = [
+    "allow_netstat_ssh: true",
+    "allow_netstat_http: true",
+    "netstats_secret: 'secret'"
+  ]
+
 }
