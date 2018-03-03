6633af49f77f1f98d627402039febcf9e52b508e
Aleskey Zalesov <aleksey.zalesov@altoros.com>
2018-03-03 18:03:49 +0300

    Update main.tf for minimal configuration

--- azure-refactored/main.tf
+++ azure-refactored/main.tf
@@ -1,22 +1,9 @@
 # Configure the Azure Provider
+# To recieve token run `az login` from console
 provider "azurerm" {
   version = "1.1.2"
 }
 
-module "common" {
-  source = "./poanetwork/modules/common"
-}
-
-module "bootnode" {
-  source = "./poanetwork/modules/bootnode"
-
-  resource_group_name = "${module.common.resource_group_name}"
-  subnet_id = "${module.common.subnet_id}"
-
-  network_name = "sokol"
-  platform = "centos"
-  role = "bootnode"
-
-  node_admin_email = "admin@example.com"
-  node_name = "fly"
+module "poa" {
+  source = "./poanetwork"
 }
