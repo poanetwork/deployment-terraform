67425c64c17459365d227c848ed13ea594c970a4
Aleskey Zalesov <aleksey.zalesov@altoros.com>
2018-03-03 17:33:00 +0300

    Refactor moc module

--- azure-refactored/examples/moc/main.tf
+++ azure-refactored/examples/moc/main.tf
@@ -8,8 +8,17 @@ module "common" {
 }
 
 module "moc" {
-  source = "./poanetwork/modules/moc"
+  source = "./poanetwork/modules/node"
 
   resource_group_name = "${module.common.resource_group_name}"
   subnet_id = "${module.common.subnet_id}"
+
+  network_name = "sokol"
+  platform = "ubuntu"
+  role = "moc"
+
+  config = [
+    "moc_keypass: 'secret'",
+    "moc_keyfile: '~/.ssh/id_poa-test.pub'"
+  ]
 }
