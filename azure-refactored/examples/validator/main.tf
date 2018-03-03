16e84ced16b0fc5eda4d0cacf8c7b1236efa71d6
Aleskey Zalesov <aleksey.zalesov@altoros.com>
2018-03-03 17:35:44 +0300

    Refactor validator module

--- azure-refactored/examples/validator/main.tf
+++ azure-refactored/examples/validator/main.tf
@@ -8,8 +8,20 @@ module "common" {
 }
 
 module "validator" {
-  source = "./poanetwork/modules/validator"
+  source = "./poanetwork/modules/node"
 
   resource_group_name = "${module.common.resource_group_name}"
   subnet_id = "${module.common.subnet_id}"
+
+  network_name = "sokol"
+  platform = "ubuntu"
+  role = "validator"
+
+  config = [
+    "allow_validator_ssh: true",
+    "allow_validator_p2p: true",
+    "mining_keyfile": "~/.ssh/id_poa-test.pub"
+    "mining_address": "0xABC"
+    "mining_keypass": "secret"
+  ]
 }
