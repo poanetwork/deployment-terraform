3a49b0068e46ec4e7c702f352a3f6dd2dedc357b
Aleskey Zalesov <aleksey.zalesov@altoros.com>
2018-03-03 11:40:29 +0300

    Add bootnode module

--- azure-refactored/main.tf
+++ azure-refactored/main.tf
@@ -6,3 +6,10 @@ provider "azurerm" {
 module "common" {
   source = "./poanetwork/modules/common"
 }
+
+module "bootnode" {
+  source = "./poanetwork/modules/bootnode"
+
+  resource_group_name = "${module.common.resource_group_name}"
+  subnet_id = "${module.common.subnet_id}"
+}
---
+++ azure-refactored/poanetwork/modules/common/outputs.tf
@@ -0,0 +1,9 @@
+output "resource_group_name" {
+  description = "Name of the created resource group"
+  value = "${azurerm_resource_group.poa.name}"
+}
+
+output "subnet_id" {
+  description = "Subnet ID"
+  value = "${azurerm_subnet.poa.id}"
+}
