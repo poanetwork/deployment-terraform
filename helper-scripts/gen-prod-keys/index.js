4b1596833d86713c60d1cf9acce529235a07c378
phahulin <p.hahulin@hintsolutions.ru>
2018-05-31 17:12:39 +0300

    Make gen-prod-keys script process all initial keys from keystore folder

--- helper-scripts/gen-prod-keys/index.js
+++ helper-scripts/gen-prod-keys/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 const Keythereum = require('keythereum');
 const EthereumTx = require('ethereumjs-tx');
 const Web3 = require('web3');
@@ -9,11 +10,11 @@ const mkdirp = require('mkdirp');
 
 const RPC_ENDPOINT = process.env.RPC_ENDPOINT || 'http://localhost:8545';
 const web3 = new Web3(new Web3.providers.HttpProvider(RPC_ENDPOINT));
-const keysDir = process.argv[2];
-const address = process.argv[3];
-const password = process.argv[4];
-const keysManagerAddress = process.argv[5];
+const keysDir = '.';
+const keysManagerAddress = process.argv[2];
 const keysManagerABI = require('./KeysManager.abi.json');
+var keysManagerContract;
+var chainId;
 
 const GASPRICE_GWEI = 2;
 
@@ -25,7 +26,7 @@ function logerr(...args) {
     console.error(new Date().toISOString(), ...args);
 }
 
-function generateKey() {
+function generateKey(name) {
     var params = {
         keyBytes: 32,
         ivBytes: 16,
@@ -34,6 +35,7 @@ function generateKey() {
     var password = GeneratePassword(20, false);
     var keyObj = Keythereum.dump(password, dk.privateKey, dk.salt, dk.iv, {});
     return {
+        name,
         dk,
         password,
         keyObj,
@@ -41,14 +43,8 @@ function generateKey() {
 }
 
 function validateArgs() {
-    if (process.argv.length !== 6) {
+    if (process.argv.length !== 3) {
         logerr('Usage: node index.js KEYSTORE_DIR 0xADDRESS KEYSTORE_PASSWORD KEYS_MANAGER_ADDRESS');
-        process.exit(1);
-    }
-
-    if (!web3.utils.isAddress(address)) {
-        logerr('Incorrect parameter ADDRESS: ' + address);
-        logerr('Expecting 0x-prefixed address. If it contains both uppercase and lowercase letters, checksum is also validated.');
         process.exit(1);
     }
 
@@ -59,128 +55,136 @@ function validateArgs() {
     }
 }
 
-function getPrivateKeyFromInitialKey(address, keysDir) {
-    var key = Keythereum.importFromFile(address, keysDir);
-    var privateKey = Keythereum.recover(password, key);
-    return privateKey;
+function getInitialKey(address) {
+    var keyObj = Keythereum.importFromFile(address, keysDir);
+    var password = fs.readFileSync(path.join(keysDir, './keystore', `${address}.key`), 'utf8');
+    return {
+        name: 'initial',
+        keyObj,
+        password
+    };
 }
 
-function createKeys(address, privateKey, keysManagerContract, miningKeyAddress, votingKeyAddress, payoutKeyAddress, callback) {
-    loginf('createKeys for: ' + address);
-    loginf('calling getId');
-    web3.eth.net.getId((err, chainId) => {
+function getPrivateKey(key) {
+    return Keythereum.recover(key.password, key.keyObj);
+}
+
+function createKeys(address, privateKey, miningKeyAddress, votingKeyAddress, payoutKeyAddress, callback) {
+    loginf('*** createKeys for: ' + address);
+    loginf('calling estimateGas');
+    keysManagerContract.methods.createKeys(miningKeyAddress, votingKeyAddress, payoutKeyAddress).estimateGas({ from: address }, (err, gasEstimation) => {
         if (err) throw err;
 
-        loginf('chainId:', chainId);
-        loginf('calling estimateGas');
-        keysManagerContract.methods.createKeys(miningKeyAddress, votingKeyAddress, payoutKeyAddress).estimateGas({ from: address }, (err, gasEstimation) => {
+        loginf('gasEstimation:', gasEstimation);
+        loginf('calling getTransactionCount');
+        /*
+        web3.eth.getTransactionCount(address, (err, nonce) => {
             if (err) throw err;
 
-            loginf('gasEstimation:', gasEstimation);
-            loginf('calling getTransactionCount');
-            web3.eth.getTransactionCount(address, (err, nonce) => {
-                if (err) throw err;
+            loginf('nonce:', nonce);
+            loginf('encoding method call');
+            var txData = keysManagerContract.methods.createKeys(miningKeyAddress, votingKeyAddress, payoutKeyAddress).encodeABI();
+            loginf('txData:', txData);
+            var txParams = {
+                from: `0x${address}`,
+                to: keysManagerAddress,
+                data: txData,
+                gasPrice: web3.utils.toHex(web3.utils.toWei(GASPRICE_GWEI.toString(), 'gwei')),
+                gasLimit: parseInt(gasEstimation),
+                chainId: chainId,
+                nonce: parseInt(nonce),
+            };
+            loginf('txParams:', txParams);
 
-                loginf('nonce:', nonce);
-                loginf('encoding method call');
-                var txData = keysManagerContract.methods.createKeys(miningKeyAddress, votingKeyAddress, payoutKeyAddress).encodeABI();
-                loginf('txData:', txData);
-                var txParams = {
-                    from: address,
-                    to: keysManagerAddress,
-                    data: txData,
-                    gasPrice: web3.utils.toHex(web3.utils.toWei(GASPRICE_GWEI.toString(), 'gwei')),
-                    gasLimit: parseInt(gasEstimation),
-                    chainId: chainId,
-                    nonce: parseInt(nonce),
-                };
+            loginf('signing tx');
+            var tx = new EthereumTx(txParams);
+            tx.sign(privateKey);
+            var serializedTx = tx.serialize();
+            loginf('calling sendSignedTransaction');
 
-                console.log('txParams:', txParams);
-
-                loginf('signing tx');
-                var tx = new EthereumTx(txParams);
-                tx.sign(privateKey);
-                var serializedTx = tx.serialize();
-                loginf('calling sendSignedTransaction');
-                web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'), (err, txHash) => {
-                    if (err) throw err;
-
-                    loginf('txHash:', txHash);
-                    return callback(null, txHash);
-                });
-            });
+            web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+            .on('transactionHash', txHash => loginf('got txHash:', txHash))
+            .on('receipt', txReceipt => { loginf('got txReceipt:', txReceipt); callback(null, txReceipt); })
+            .on('error', err => callback(err));
         });
+        */
+        setTimeout(callback, 5000);
     });
+}
+
+function asyncForEach(array, forEach, done) {
+    if (array.length === 0) return done();
+
+    var i = -1;
+    function next(err) {
+        if (err) return done(err);
+        if (i === array.length - 1) return done();
+        i += 1;
+        setTimeout(function () { forEach(array[i], next) }, 0);
+    }
+    next();
+}
+
+function saveKey(folder, key) {
+    loginf(`* saving ${key.name} key:`, key.keyObj.address);
+    loginf('* password:', key.password);
+    loginf('* json keystore:', JSON.stringify(key.keyObj));
+    fs.writeFileSync(`${folder}/${key.name}_${key.keyObj.address}.json`, JSON.stringify(key.keyObj));
+    fs.writeFileSync(`${folder}/${key.name}_${key.keyObj.address}.key`, key.password);
+}
+
+function processInitialKeyFile(fname, done) {
+    loginf('***** processing:', fname);
+    var address = path.parse(fname).name;
+    loginf('address:', address);
+    if (!web3.utils.isAddress(`0x${address}`)) {
+        logerr('Unexpected json keystore file name: ' + fname);
+        process.exit(1);
+    }
+    var initialKey = getInitialKey(address, keysDir);
+    var privateKey = getPrivateKey(initialKey);
+    loginf('Recovered private key from initial key:', privateKey.toString('hex'));
+
+    var miningKey = generateKey('mining');
+    var votingKey = generateKey('voting');
+    var payoutKey = generateKey('payout');
+    loginf('miningKey:', miningKey);
+    loginf('votingKey:', votingKey);
+    loginf('payoutKey:', payoutKey);
+
+    createKeys(
+        address,
+        privateKey,
+        miningKey.keyObj.address,
+        votingKey.keyObj.address,
+        payoutKey.keyObj.address,
+        (err, txReceipt) => {
+            if (err) throw err;
+
+            var folder = `./production-keys/validator-${address}/`;
+            loginf('Creating keys folder:', folder);
+            mkdirp(folder);
+
+            saveKey(folder, initialKey);
+            saveKey(folder, miningKey);
+            saveKey(folder, votingKey);
+            saveKey(folder, payoutKey);
+
+            loginf('***** done with', fname);
+            return done();
+        }
+    );
 }
 
 // ********** MAIN ********* //
 validateArgs();
+keysManagerContract = new web3.eth.Contract(keysManagerABI, keysManagerAddress);
 
-var miningKey = generateKey();
-var votingKey = generateKey();
-var payoutKey = generateKey();
-
-loginf('miningKey:', miningKey);
-loginf('votingKey:', votingKey);
-loginf('payoutKey:', payoutKey);
-
-const keysManagerContract = new web3.eth.Contract(keysManagerABI, keysManagerAddress);
-var privateKey = getPrivateKeyFromInitialKey(address, keysDir);
-loginf('Recovered private key from initial key: ' + privateKey.toString('hex'));
-
-createKeys(
-    address,
-    privateKey,
-    keysManagerContract,
-    miningKey.keyObj.address,
-    votingKey.keyObj.address,
-    payoutKey.keyObj.address,
-    (err, txHash) => {
-        if (err) throw err;
-
-        var folder = `./production-keys/validator-${address}/`;
-        loginf('Creating keys folder:', folder);
-        mkdirp(folder);
-
-        loginf('* saving miningKey:', miningKey.keyObj.address);
-        loginf('* password:', miningKey.password);
-        loginf('* json keystore:', JSON.stringify(miningKey.keyObj));
-        fs.writeFileSync(
-            `./production-keys/validator-0x${address}/mining-${miningKey.keyObj.address}.json`,
-            JSON.stringify(miningKey.keyObj)
-        );
-        fs.writeFileSync(
-            `./production-keys/validator-0x${address}/mining-${miningKey.keyObj.address}.key`,
-            miningKey.password
-        );
-        // Keythereum.exportToFile(miningKey.keyObj);
-
-        loginf('* saving votingKey:', votingKey.keyObj.address);
-        loginf('* password:', votingKey.password);
-        loginf('* json keystore:', JSON.stringify(votingKey.keyObj));
-        fs.writeFileSync(
-            `./production-keys/validator-0x${address}/voting-${votingKey.keyObj.address}.json`,
-            JSON.stringify(votingKey.keyObj)
-        );
-        fs.writeFileSync(
-            `./production-keys/validator-0x${address}/voting-${votingKey.keyObj.address}.key`,
-            votingKey.password
-        );
-        // Keythereum.exportToFile(votingKey.keyObj);
-
-        loginf('* saving payoutKey:', payoutKey.keyObj.address);
-        loginf('* password:', payoutKey.password);
-        loginf('* json keystore:', JSON.stringify(payoutKey.keyObj));
-        fs.writeFileSync(
-            `./production-keys/validator-0x${address}/payout-${payoutKey.keyObj.address}.json`,
-            JSON.stringify(payoutKey.keyObj)
-        );
-        fs.writeFileSync(
-            `./production-keys/validator-0x${address}/payout-${payoutKey.keyObj.address}.key`,
-            payoutKey.password
-        );
-        // Keythereum.exportToFile(payoutKey.keyObj);
-
-        loginf('Done');
-    }
-);
+loginf('calling getId');
+web3.eth.net.getId((err, _chainId) => {
+    if (err) throw err;
+    chainId = _chainId;
+    var allKeys = fs.readdirSync(path.join(keysDir, './keystore')).filter(fname => !fname.startsWith('.') && !fname.startsWith('_') & fname.endsWith('.json'));
+    loginf('allKeys:', allKeys);
+    asyncForEach(allKeys, processInitialKeyFile, () => loginf('Completed'));
+});
