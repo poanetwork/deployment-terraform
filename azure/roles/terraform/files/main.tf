a8440411a6e969068e2d6b4cc350da54314cdc01
ArseniiPetrovich <arsenii.petrovich@altoros.com>
2018-07-08 19:56:23 +0300

    Removed hardcode

--- azure/roles/terraform/files/main.tf
+++ azure/roles/terraform/files/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_resource_group" "poa" {
 
 # Create virtual network
 resource "azurerm_virtual_network" "poa" {
-  name                = "${var.prefix}poa-network"
+  name                = "${var.prefix}${var.network_name}-network"
   address_space       = ["10.0.0.0/16"]
   location            = "${var.region}"
   resource_group_name = "${azurerm_resource_group.poa.count > 0 ? element(concat(azurerm_resource_group.poa.*.name, list("")), 0) : var.resource_group_name}"
@@ -28,7 +28,7 @@ resource "azurerm_virtual_network" "poa" {
 
 # Create subnet
 resource "azurerm_subnet" "poa" {
-  name                 = "${var.prefix}poa-subnet"
+  name                 = "${var.prefix}${var.network_name}-subnet"
   resource_group_name  = "${azurerm_resource_group.poa.count > 0 ? element(concat(azurerm_resource_group.poa.*.name, list("")), 0) : var.resource_group_name}"
   virtual_network_name = "${azurerm_virtual_network.poa.name}"
   address_prefix       = "10.0.1.0/24"
